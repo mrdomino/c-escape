aaa1a655c613cd631ed4bf1fdba924949f8d7b21
Steven Dee <steve@smartercode.net>
2015-03-23 15:34:50 -0400

    tsst

--- c-escape.c
+++ c-escape.c
@@ -13,37 +13,26 @@ usage(void)
 	exit(1);
 }
 
-static size_t
-c_escape(char c, char out[static 5])
+static const char*
+c_escape(char c)
 {
-	int r;
+	static char buf[5];
 
-	if (iscntrl(c) || !isascii(c))
-		r = snprintf(out, 5, "\\x%02hhx", c);
-	else {
-		if (c == '\"' || c == '\\')
-			r = snprintf(out, 5, "\\%c", c);
-		else {
-			out[0] = c;
-			out[1] = '\0';
-			r = 1;
-		}
-	}
-	assert(r > 0 && r < 5);
-	return (size_t)r;
+	if (iscntrl(c) || !isascii(c) || c == '\"' || c == '\\')
+		(void) snprintf(buf, 5, "\\x%02hhx", c);
+	else
+		buf[0] = c, buf[1] = '\0';
+	return buf;
 }
 
 static void
 c_escape_concat(FILE* out, FILE* in)
 {
-	char buf[5];
 	int c;
 
-	while ((c = fgetc(in)) != EOF) {
-		(void) c_escape((char)c, buf);
-		if (fputs(buf, out) != 0)
+	while ((c = fgetc(in)) != EOF)
+		if (fputs(c_escape((char)c), out))
 			err(1, "fputs");
-	}
 }
 
 int
